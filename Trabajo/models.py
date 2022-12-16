0c89be1a4db788d5dfa7ea86c100c493326a36cb
Emanuel Huerga <emanuel_cehc_95@hotmail.com>
2022-12-16 17:33:49 -0300

    models

--- Trabajo/models.py
+++ Trabajo/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from datetime import date
+from django.utils import timezone
 
 # Create your models here.
 class Trabajo(models.Model):
     nombre = models.CharField(max_length=50,blank=False,null=False)
     descripcion = models.CharField(max_length=50)
-    fecha_desde = models.DateField(default=date.today(),null=False)
+    fecha_desde = models.DateField(default=timezone.now,null=False)
     fecha_hasta = models.DateField(null=True,blank=True)
 
     def __str__(self):
